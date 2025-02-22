f2059345776ba801c35df1b0df7d527c276ad06a
Hilal Ahmad <115874624+hilalahmad0101@users.noreply.github.com>
2025-02-22 02:03:22 -0800

    feat: invalid tweet

---
+++ 05_invalid_tweet.sql
@@ -0,0 +1,28 @@
+-- Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+-- Input: 
+-- Tweets table:
+-- +----------+-----------------------------------+
+-- | tweet_id | content                           |
+-- +----------+-----------------------------------+
+-- | 1        | Let us Code                       |
+-- | 2        | More than fifteen chars are here! |
+-- +----------+-----------------------------------+
+-- Output: 
+-- +----------+
+-- | tweet_id |
+-- +----------+
+-- | 2        |
+-- +----------+
+-- Explanation: 
+-- Tweet 1 has length = 11. It is a valid tweet.
+-- Tweet 2 has length = 33. It is an invalid tweet.
+
+select tweet_id from Tweets where char_length(content) > 15
+
+-- output
+
+-- Output
+-- | tweet_id |
+-- | -------- |
+-- | 2        |
