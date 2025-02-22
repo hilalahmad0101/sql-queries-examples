67f0281a7e182d74eb831a8e1805575819beabd8
Hilal Ahmad <115874624+hilalahmad0101@users.noreply.github.com>
2025-02-22 01:53:21 -0800

    feat: add output

--- recyclable-and-low-fat-products.sql
+++ recyclable-and-low-fat-products.sql
@@ -24,3 +24,14 @@
 
 
 select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
+
+
+-- case 1 passed -output
+
+-- | product_id | low_fats | recyclable |
+-- | ---------- | -------- | ---------- |
+-- | 0          | Y        | N          |
+-- | 1          | Y        | Y          |
+-- | 2          | N        | Y          |
+-- | 3          | Y        | Y          |
+-- | 4          | N        | N          |
